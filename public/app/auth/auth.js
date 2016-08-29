9dec8549f9d452d3dcfd5713a05054e64f1bd224
Yara TP <ytercero36@gmail.com>
2016-08-29 15:33:17 -0400

    Added signup method

--- public/app/auth/auth.js
+++ public/app/auth/auth.js
@@ -2,5 +2,16 @@ angular.module('app.auth', [])
 .controller('AuthController', function($scope, $location, Auth){
   angular.extend($scope, Auth);
 
+  $scope.user = {};
+
+  $scope.signup = function(){
+    Auth.signup($scope.user)
+      .then(function(){
+        $location.path('/home');
+      })
+      .catch(function(error){
+        console.error(error);
+      })
+  };
 
 });
