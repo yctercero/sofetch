e5d326f9aad3fb2beeeb2c08fb9a32b6b3405fd4
Yara TP <ytercero36@gmail.com>
2016-08-29 16:15:11 -0400

    Added routing for login/signup

--- public/app/app.js
+++ public/app/app.js
@@ -4,6 +4,21 @@ angular.module('app', [
 .config(function($stateProvider, $urlRouterProvider){
   $urlRouterProvider.otherwise('/signup');
 
-
+  $stateProvider
+  .state('home', {
+    url: '/home',
+    templateUrl: 'app/timeline/timeline.html',
+    controller: 'TimelineController'
+  })
+  .state('signup', {
+    url: '/signup',
+    templateUrl: 'app/auth/signup.html',
+    controller: 'AuthController'
+  })
+  .state('login', {
+    url: '/login',
+    templateUrl: 'app/auth/login.html',
+    controller: 'AuthController'
+  });
 
 });
